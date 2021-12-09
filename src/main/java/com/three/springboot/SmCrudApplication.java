a65f6e99548133ec2371fce6ba939fd0b302025b
SMRashid <sayyed.rashid2003@gmail.com>
2021-12-09 16:06:04 +0530

    added src

---
+++ src/main/java/com/three/springboot/SmCrudApplication.java
@@ -0,0 +1,13 @@
+package com.three.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmCrudApplication.class, args);
+	}
+
+}
